54e35cf7af4e221b166a0621d97d7e9119882111
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-02-12 11:33:19 +0530

    Initial commit

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { SignIn } from "@clerk/clerk-react";
+
+export default function Page() {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <SignIn />
+    </div>
+  );
+}
